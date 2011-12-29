2ecbb66a38da9e6326956e0beca2175521012696
Marijn Kampf <github.com@gwobbel.com>
2011-12-29 11:56:02 +0000

    Fix for [User Error] Uncaught Exception: Object->__call(): the method 'onmobiledomain' does not exist on 'Security' occurred after new install of Silverstripe 2.4.5 and Silverstripe-Mobile.

--- code/MobileSiteTreeExtension.php
+++ code/MobileSiteTreeExtension.php
@@ -8,7 +8,7 @@ class MobileSiteTreeExtension extends DataObjectDecorator {
 		$config = SiteConfig::current_site_config();
 
 		// Ensure a canonical link is placed, for semantic correctness and SEO
-		if(Controller::has_curr() && Controller::curr()->onMobileDomain() && $config->MobileSiteType == 'RedirectToDomain') {
+		if(Controller::has_curr() && Controller::curr()->hasMethod("onMobileDomain") && Controller::curr()->onMobileDomain() && $config->MobileSiteType == 'RedirectToDomain') {
 			$oldBaseURL = Director::baseURL();
 			Director::setbaseURL($config->FullSiteDomain);
 			$tags .= sprintf('<link rel="canonical" href="%s" />', $this->owner->AbsoluteLink()) . "\n";
