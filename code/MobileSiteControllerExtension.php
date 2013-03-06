silverstripe-archive/silverstripe-mobile

commit e0ba458b75830e2d95b45a7c398ad253c62aaf79

FIX: resolve MobileSiteControllerExtension test failing

code/MobileSiteControllerExtension.php
@@ -35,20 +35,24 @@ class MobileSiteControllerExtension extends Extension {
 
 		// Enforce the site (cookie expires in 30 minutes)
 		$fullSite = $request->getVar('fullSite');
+
 		if(is_numeric($fullSite)) {
-			Cookie::set('fullSite', (int)$fullSite);
+			$fullSiteCookie = (int)$fullSite;
+			Cookie::set('fullSite', $fullSiteCookie);
 
 			// use the host of the desktop version of the site to set cross-(sub)domain cookie
 			$domain = $config->FullSiteDomainNormalized;
+
 			if (!empty($domain)) {
 				Cookie::set('fullSite', $fullSite, time() + self::$cookie_expire_time, null, '.' . parse_url($domain, PHP_URL_HOST));
 			} else { // otherwise just use a normal cookie with the default domain
 				Cookie::set('fullSite', $fullSite, time() + self::$cookie_expire_time);
 			}
 		}
-
-		// Site is being forced via flag or cookie
-		$fullSiteCookie = Cookie::get('fullSite');
+		else {
+			$fullSiteCookie = Cookie::get('fullSite');
+		}
+		
 		if(is_numeric($fullSiteCookie)) {
 			// Full site requested
 			if($fullSiteCookie) {

commit 88ea660591fde127e00ed63e3868a8cce3376df2

FIX Infinite redirects on conflicting fullSite cookie domains
They were both set for .mydomain.com and m.mydomain.com,
which could lead to different values based on which domain,
ending up in the redirects infinitely ping-ponging between the two.
Regression from e0ba458b

code/MobileSiteControllerExtension.php
@@ -38,7 +38,6 @@ class MobileSiteControllerExtension extends Extension {
 
 		if(is_numeric($fullSite)) {
 			$fullSiteCookie = (int)$fullSite;
-			Cookie::set('fullSite', $fullSiteCookie);
 
 			// use the host of the desktop version of the site to set cross-(sub)domain cookie
 			$domain = $config->FullSiteDomainNormalized;