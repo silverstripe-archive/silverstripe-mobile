74cdcd05b427c0b0f89c3c335fbb7abee9cfc8b8
Sean Harvey <sean@silverstripe.com>
2014-02-18 17:21:20 +1300

    Fixing broken unit test.

--- tests/MobileSiteConfigExtensionTest.php
+++ tests/MobileSiteConfigExtensionTest.php
@@ -7,6 +7,8 @@ class MobileSiteConfigExtensionTest extends SapphireTest {
 
 	public function setUp() {
 		MobileSiteConfigExtension::set_theme_copy_path(TEMP_FOLDER . '/mobile-test-copy-theme/');
+		MobileSiteConfigExtension::copyDefaultTheme();
+
 		parent::setUp();
 	}
 
