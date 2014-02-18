50f8b766eeccc80251f11af203884f58cae92e6c
Sean Harvey <sean@silverstripe.com>
2014-02-19 09:31:24 +1300

    Fixing deprecated use of Director::setBaseURL()

--- code/MobileSiteTreeExtension.php
+++ code/MobileSiteTreeExtension.php
@@ -10,9 +10,10 @@ class MobileSiteTreeExtension extends DataExtension {
 		// Ensure a canonical link is placed, for semantic correctness and SEO
 		if(Controller::has_curr() && Controller::curr()->hasMethod("onMobileDomain") && Controller::curr()->onMobileDomain() && $config->MobileSiteType == 'RedirectToDomain') {
 			$oldBaseURL = Director::baseURL();
-			Director::setbaseURL($config->FullSiteDomain);
+
+			Config::inst()->update('Director', 'alternate_base_url', $config->FullSiteDomain);
 			$tags .= sprintf('<link rel="canonical" href="%s" />', $this->owner->AbsoluteLink()) . "\n";
-			Director::setbaseURL($oldBaseURL);
+			Config::inst()->update('Director', 'alternate_base_url', $oldBaseURL);
 		}
 	}
 	
